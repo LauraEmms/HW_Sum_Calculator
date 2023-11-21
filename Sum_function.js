1156bcd4f18223d47f553b34d51b5dcf2cc17dcf
LauraEmms <lauraelizaemms@outlook.com>
2023-11-21 22:25:18 +0000

    Formatting code with Prettier

--- Sum_function.js
+++ Sum_function.js
@@ -1,7 +1,6 @@
 function addNumbers() {
-    var Box1 = parseInt(document.getElementById("value1").value);
-    var Box2 = parseInt(document.getElementById("value2").value);
-    var result = document.getElementById("result");
-    result.value = Box1 + Box2;
-  }
-  
+  var Box1 = parseInt(document.getElementById("value1").value);
+  var Box2 = parseInt(document.getElementById("value2").value);
+  var result = document.getElementById("result");
+  result.value = Box1 + Box2;
+}
